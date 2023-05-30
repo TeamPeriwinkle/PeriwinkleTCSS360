8f0723970bcceab743f9f2e977c13bb8da396d4d
soepanda <129916607+soepanda@users.noreply.github.com>
2023-05-30 15:03:00 -0700

    Added javadoc

--- src/main/java/tcss360/diybuilder/models/Item.java
+++ src/main/java/tcss360/diybuilder/models/Item.java
@@ -1,43 +1,95 @@
+/*
+ * Team Periwinkle
+ */
 package tcss360.diybuilder.models;
 
+/**
+ * Item Object class.
+ *
+ * @author Soe Lin
+ */
 public class Item {
 
+    /** Name of the item. */
     private String name;
 
+    /** Price of the item. */
     private double price;
-
+    /** Total unit of the item. */
     private int unit;
 
+    /**
+     * Constructor.
+     *
+     * @param theName
+     * @param thePrice
+     * @param theUnit
+     */
     public Item(String theName, double thePrice, int theUnit) {
         name = theName;
         price = thePrice;
         unit = theUnit;
     }
 
+    /**
+     * Return the name of the item.
+     *
+     * @return name of the item
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Return the price of the item.
+     *
+     * @return price of the item
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Return the total unit of the item.
+     *
+     * @return total unit of the item
+     */
     public int getUnit() {
         return unit;
     }
 
+    /**
+     * Set the name of the item.
+     *
+     * @param theName
+     */
     public void setName(String theName) {
         name = theName;
     }
 
+    /**
+     * Set the price of the item.
+     *
+     * @param thePrice
+     */
     public void setPrice(double thePrice) {
         price = thePrice;
     }
 
+    /**
+     * Set the total unit of the item.
+     *
+     * @param theUnit
+     */
     public void setUnit(int theUnit) {
         unit = theUnit;
     }
 
+    /**
+     * Return the total cost based on the total unit.
+     *
+     * @return total cost
+     */
     public double getTotalCost() {
         return price * unit;
     }
