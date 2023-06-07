62a69c9c0a2761365c0b32ba6cf8d6d2a7837fb0
Alex Garcia <alexgarcia28098@gmail.com>
2023-06-07 15:45:49 -0700

    removed some tests as they were outdated

--- src/test/java/tcss360/diybuilder/AppTest.java
+++ src/test/java/tcss360/diybuilder/AppTest.java
@@ -20,125 +20,144 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-
-    /**
-     * read data from json file
-     *  @author Alex Garcia
-     */
-    @Test
-    public void readinusers() throws URISyntaxException {
-        UserController controller = new UserController();
-        //System.out.println(controller.data.get("users"));
-    }
-
-    /**
-     * read data from json file and generate a User object
-     * @Author Alex Garcia
-     */
-    @Test
-    public void createUser() throws URISyntaxException {
-        UserController controller = new UserController();
-        User me = controller.getUserObject("alexg123");
-        String expectedName = "alexg123";
-        String expectedEmail ="alex123@gmail.com";
-        String actualName = me.getUserName();
-        String actualEmail = me.getEmail();
-
-        assertTrue(expectedName.equals(actualName));
-        assertTrue(expectedEmail.equals(actualEmail));
-
-    }
-
-    /**
-     * Export and import a user Object for Iteration 2(serialize and deserialize)
-     * @author Alex Garcia
-     */
-    @Test
-    public void iteration2Test() throws IOException, ClassNotFoundException {
-
-    }
-
-    @Test
-    public void loginUser(){
-        Controller genController = new Controller();
-
-        String username = "a6";
-        String password = "a6";
-
-        UserController userController = new UserController();
-        UserController.loadUserAccount(username);
-        assertTrue(userController.checkCredentials(username, password));
-
-        User testUser = userController.getUserObject(username);
-
-    }
-
-
-    /**
-     * test to see if new info is added to permanent data
-     * WORKS!!!:) somehow got it to run without error on the first atttempt
-     */
-    @Test
-    public void createProjectWithItems(){
-//        Controller c = new Controller();
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+//
+//    /**
+//     * read data from json file
+//     *  @author Alex Garcia
+//     */
+//    @Test
+//    public void readinusers() throws URISyntaxException {
+//        UserController controller = new UserController();
+//        //System.out.println(controller.data.get("users"));
+//    }
+//
+//    /**
+//     * read data from json file and generate a User object
+//     * @Author Alex Garcia
+//     */
+//    @Test
+//    public void createUser() throws URISyntaxException {
+//        UserController controller = new UserController();
+//        User me = controller.getUserObject("alexg123");
+//        String expectedName = "alexg123";
+//        String expectedEmail ="alex123@gmail.com";
+//        String actualName = me.getUserName();
+//        String actualEmail = me.getEmail();
+//
+//        assertTrue(expectedName.equals(actualName));
+//        assertTrue(expectedEmail.equals(actualEmail));
+//
+//    }
+//
+//    /**
+//     * Export and import a user Object for Iteration 2(serialize and deserialize)
+//     * @author Alex Garcia
+//     */
+//    @Test
+//    public void iteration2Test() throws IOException, ClassNotFoundException {
+//
+//    }
+//
+//    @Test
+//    public void loginUser(){
+//        Controller genController = new Controller();
+//
+//        String username = "a6";
+//        String password = "a6";
+//
+//        UserController userController = new UserController();
+//        UserController.loadUserAccount(username);
+//        assertTrue(userController.checkCredentials(username, password));
+//
+//        User testUser = userController.getUserObject(username);
+//
+//    }
+//
+//
+//    /**
+//     * test to see if new info is added to permanent data
+//     * WORKS!!!:) somehow got it to run without error on the first atttempt
+//     */
+//    @Test
+//    public void createProjectWithItems(){
+////        Controller c = new Controller();
+////        String username = "alexg123";
+////        String password = "123";
+////        loadUserData();
+////
+////        UserController.loadUserAccount(username);
+////
+////        String projectName = "gaming pc";
+////        String description = "new pc build for gaming and school";
+////        Double budget = 1200.0;
+////
+////        Project newProj = new Project(projectName, budget,description);
+////
+////        ProjectController.createProject(username, newProj);
+////
+////        ProjectController.loadProject(projectName);
+////
+////        ProjectController.createTask("Gather Parts");
+////        ProjectController.loadTask("Gather Parts");
+////
+////        ProjectController.createItem("CPU", 120.0, 1);
+////
+////        ProjectController.loadItem("CPU");
+//    }
+//
+//    /**
+//     * try to delete the items, and tasks added in the previous test
+//     * WORKS!!!:) somehow got it to run without error on the first atttempt
+//     */
+//    @Test
+//    public void deleteItem(){
+//        Controller c = new Controller();//can have this be done for all tests but for now it will be a part of every test
+//        UserController.loadUserData();
+//
 //        String username = "alexg123";
-//        String password = "123";
-//        loadUserData();
 //
 //        UserController.loadUserAccount(username);
 //
+//
+//        //items and projects to be deleted for testing
 //        String projectName = "gaming pc";
-//        String description = "new pc build for gaming and school";
-//        Double budget = 1200.0;
-//
-//        Project newProj = new Project(projectName, budget,description);
-//
-//        ProjectController.createProject(username, newProj);
+//        String taskName = "Gather Parts";
+//        String itemName = "CPU";
 //
 //        ProjectController.loadProject(projectName);
 //
-//        ProjectController.createTask("Gather Parts");
-//        ProjectController.loadTask("Gather Parts");
+//        ProjectController.loadTask(taskName);
 //
-//        ProjectController.createItem("CPU", 120.0, 1);
+//        //should delete the item
+//        ProjectController.deleteItem(itemName);
+//    }
 //
-//        ProjectController.loadItem("CPU");
-    }
-
-    /**
-     * try to delete the items, and tasks added in the previous test
-     * WORKS!!!:) somehow got it to run without error on the first atttempt
-     */
-    @Test
-    public void deleteItem(){
-        Controller c = new Controller();//can have this be done for all tests but for now it will be a part of every test
-        UserController.loadUserData();
-
-        String username = "alexg123";
-
-        UserController.loadUserAccount(username);
-
-
-        //items and projects to be deleted for testing
-        String projectName = "gaming pc";
-        String taskName = "Gather Parts";
-        String itemName = "CPU";
-
-        ProjectController.loadProject(projectName);
-
-        ProjectController.loadTask(taskName);
-
-        //should delete the item
-        ProjectController.deleteItem(itemName);
-    }
-
-    @Test
-    public void deleteTask(){
+//    @Test
+//    public void deleteTask(){
+////        Controller c = new Controller();//can have this be done for all tests but for now it will be a part of every test
+////        UserController.loadUserData();
+////
+////        String username = "alexg123";
+////
+////        UserController.loadUserAccount(username);
+////
+////        //items and projects to be deleted for testing
+////        String projectName = "gaming pc";
+////        String taskName = "Gather Parts";
+////
+////        ProjectController.loadProject(projectName);
+////
+////        //should delete the item
+////        ProjectController.deleteTask(taskName);
+//    }
+//
+//    @Test
+//    public void deleteProject(){
 //        Controller c = new Controller();//can have this be done for all tests but for now it will be a part of every test
 //        UserController.loadUserData();
 //
@@ -148,27 +167,8 @@ public class AppTest
 //
 //        //items and projects to be deleted for testing
 //        String projectName = "gaming pc";
-//        String taskName = "Gather Parts";
-//
-//        ProjectController.loadProject(projectName);
 //
 //        //should delete the item
-//        ProjectController.deleteTask(taskName);
-    }
-
-    @Test
-    public void deleteProject(){
-        Controller c = new Controller();//can have this be done for all tests but for now it will be a part of every test
-        UserController.loadUserData();
-
-        String username = "alexg123";
-
-        UserController.loadUserAccount(username);
-
-        //items and projects to be deleted for testing
-        String projectName = "gaming pc";
-
-        //should delete the item
-        ProjectController.deleteProject(projectName);
-    }
+//        ProjectController.deleteProject(projectName);
+//    }
 }
